8e83c7fd6d64f51bfad9518159505d9fc562fe51
Johan Meneses <johanmeci03@gmail.com>
2024-07-09 22:55:49 -0500

    Update button and getStarted styles

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -3,12 +3,14 @@ import styles from "./style.module.css";
 
 interface ButtonProps {
   text: string;
+  icon: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ text, icon, ...rest }) => {
   return (
     <button className={styles.button} type="button" {...rest}>
       {text}
+      {icon}
     </button>
   )
 }
--- src/app/components/getStarted/getStarted.tsx
+++ src/app/components/getStarted/getStarted.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "../button/button";
+import styles from "./style.module.css";
+import ArrowIcon from "../icons/ArrowIcon";
 
 interface GetStartedProps {
   text: string;
@@ -8,9 +10,15 @@ interface GetStartedProps {
 
 const GetStarted: React.FC<GetStartedProps> = ({ text, label }) => {
   return (
-    <div>
-      <h3>{text}</h3>
-      <Button text={label} />
+    <div className={styles.sectionGetStarted}>
+      <h3 className={styles.text}>{text}</h3>
+      <div className={styles.containerForm}>
+        <div className={styles.formControl}>
+          <label htmlFor="email">Email address</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <Button text={label} icon={<ArrowIcon/>} />
+      </div>
     </div>
   )
 }
